acefe50ccf6b2f76d562e3f277c5e8719213c871
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2023-05-14 12:12:52 -0300

    Update server dockerfile and database config

--- solution/transactions-server/config/database.config.ts
+++ solution/transactions-server/config/database.config.ts
@@ -3,9 +3,18 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize('transactions-exercise', 'root', process.env.DB_PASSWORD, {
+const dbConfig = {
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD,
+  name: process.env.DB_NAME || 'transactions-exercise',
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306
+};
+
+const sequelize = new Sequelize(dbConfig.name, dbConfig.user, dbConfig.password, {
   dialect: 'mysql',
-  host: 'localhost',
+  host: dbConfig.host,
+  port: dbConfig.port
 });
 
 export default sequelize;
